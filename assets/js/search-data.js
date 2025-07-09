38699a3f67ee3d9b4514cb06cdad8efb20415648
chirag-w <54585607+chirag-w@users.noreply.github.com>
2025-07-09 00:09:46 +0000

    Deploying to gh-pages from @ chirag-w/chirag-w.github.io@2a235fe0ce7f4906e24e9e0d74b889fc6c4295db 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
-          title: "Publications",
+  },{id: "nav-papers",
+          title: "Papers",
           description: "",
           section: "Navigation",
           handler: () => {
